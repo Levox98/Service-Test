3a10d05a9c6d8e6de85a010d45ffdecec04ada22
Levox98 <ivanov.ks101@gmail.com>
2022-09-05 22:59:39 +0500

    Implemented foreground service

--- app/src/main/java/com/levox/servicetest/MainActivity.kt
+++ app/src/main/java/com/levox/servicetest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.levox.servicetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.levox.servicetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            startService(
+                MyService.newIntent(this)
+            )
+        }
+        binding.foregroundService.setOnClickListener {
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
+            )
         }
     }
 }
